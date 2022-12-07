aa5dcccbb5e3c82a7a1b0447b8dff3c1549a59c1
mzaiter <mzaiter@netways.com>
2022-12-07 17:49:08 +0200

    +add intitial projects

---
+++ LetsMeet.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetsMeet.WebApi.Controllers
+{
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
