d7cea9c1d42166711ed22a7248ab27fd847e81ca
mzaiter <mzaiter@netways.com>
2022-12-07 17:41:21 +0200

    +delete tests obejcts

--- LetsMeet.Database/Tables/Student.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Student]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [StudentId] NCHAR(10) NULL
-)
--- LetsMeet.Database/Tables/Teacher.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Teacher]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [TeacherId] NCHAR(10) NULL, 
-    [TeacherName] NCHAR(10) NULL
-)
